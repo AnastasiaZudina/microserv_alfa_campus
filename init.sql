5bd085717207a4526399998abe84f7a4bc4cbc31
AnastasiaZudina <108816201+AnastasiaZudina@users.noreply.github.com>
2022-09-11 22:22:37 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -6,15 +6,14 @@ CREATE SCHEMA IF NOT EXISTS users_scheme;
 
 --Создание таблиц
 CREATE TABLE IF NOT EXISTS  users_scheme.user(
-    ID uuid PRIMARY KEY NOT NULL,
+    Id uuid PRIMARY KEY NOT NULL,
     DELETED bit,
     BEDGA TIMESTAMP NOT NULL,
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.user_pers_info(
-    ID uuid PRIMARY KEY NOT NULL,
-    USER_ID uuid NOT NULL,
+CREATE TABLE IF NOT EXISTS  users_scheme.UserInfo(
+    Id int PRIMARY KEY NOT NULL,
     NAME varchar(120) NOT null,
     FAM varchar(120) NOT null,
     OTCH varchar(120),
@@ -26,68 +25,99 @@ CREATE TABLE IF NOT EXISTS  users_scheme.user_pers_info(
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.user_contacts(
-    ID uuid PRIMARY KEY NOT NULL,
-    USER_ID uuid NOT NULL,    
+CREATE TABLE IF NOT EXISTS  users_scheme.UserContacts(
+    Id int PRIMARY KEY NOT NULL,    
     NICKNAME varchar(120),
-    PHONE_NUM varchar (20) NOT NULL,
-    MAIL_ADDRESS varchar(120) NOT NULL,
+    phoneNum varchar (20) NOT NULL,
+    mailAddress varchar(120) NOT NULL,
     DELETED bit,
     BEDGA TIMESTAMP NOT NULL,
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.user_pics(
-    USER_ID uuid PRIMARY KEY NOT NULL,
-    UPICK varchar(500)
+CREATE TABLE IF NOT EXISTS  users_scheme.UserPics(
+    Id int PRIMARY KEY NOT NULL,
+    UPIC varchar(500)
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.user_skills(
-    ID uuid PRIMARY KEY NOT NULL,  
-    USER_ID uuid NOT NULL,
+CREATE TABLE IF NOT EXISTS  users_scheme.UserSkills(
+    Id int PRIMARY KEY NOT NULL,  
     USKILL varchar(120)
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.user_texts(
-    USER_ID uuid PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS  users_scheme.UserTexts(
+    Id int PRIMARY KEY NOT NULL,
     UTEXT varchar(1000)
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.user_relations(
-    IDPREV uuid NOT NULL,
-    IDNEXT uuid NOT NULL,
+CREATE TABLE IF NOT EXISTS  users_scheme.Subscriptions(
+    UserPrev int NOT NULL,
+    UserNext int NOT NULL,
     LKTYPE varchar(6) NOT NULL,
     DELETED bit,
     BEDGA TIMESTAMP NOT NULL,
     ENDDA TIMESTAMP NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS users_scheme.gender(
+    ID varchar (1) PRIMARY KEY NOT NULL,
+    G_TEXT varchar(10) NOT NULL
+    
+);    
 
---Идексы таблицы companies_addresses
-ALTER TABLE users_scheme.user_relations
-    ADD FOREIGN KEY (IDPREV)
-        REFERENCES users_scheme.user(id);
+--Ограничения
+ALTER TABLE users_scheme.subcriptions
+    ADD FOREIGN KEY (UserPrev)
+        REFERENCES users_scheme.user(id)
+        ON DELETE NO ACTION;
         
-ALTER TABLE users_scheme.user_relations
-    ADD FOREIGN KEY (IDNEXT)
-        REFERENCES users_scheme.user(id);
+ALTER TABLE users_scheme.subcriptions
+    ADD FOREIGN KEY (UserNext)
+        REFERENCES users_scheme.user(id)
+        ON DELETE NO ACTION;
 
-ALTER TABLE users_scheme.user_pers_info
-    ADD FOREIGN KEY (USER_ID)
-        REFERENCES users_scheme.user(id);
+ALTER TABLE users_scheme.userInfo
+    ADD FOREIGN KEY (ID)
+        REFERENCES users_scheme.user(id)
+        ON DELETE NO ACTION;
 
-ALTER TABLE users_scheme.user_contacts
-    ADD FOREIGN KEY (USER_ID)
-        REFERENCES users_scheme.user(id);
+ALTER TABLE users_scheme.userContacts
+    ADD FOREIGN KEY (ID)
+        REFERENCES users_scheme.user(id)
+        ON DELETE NO ACTION;
         
-ALTER TABLE users_scheme.user_pics
-    ADD FOREIGN KEY (USER_ID)
-        REFERENCES users_scheme.user(id);     
+ALTER TABLE users_scheme.userPics
+    ADD FOREIGN KEY (ID)
+        REFERENCES users_scheme.user(id)
+        ON DELETE CASCADE;     
 
-ALTER TABLE users_scheme.user_texts
-    ADD FOREIGN KEY (USER_ID)
-        REFERENCES users_scheme.user(id); 
+ALTER TABLE users_scheme.userTexts
+    ADD FOREIGN KEY (ID)
+        REFERENCES users_scheme.user(id)
+        ON DELETE CASCADE; 
         
-ALTER TABLE users_scheme.user_skills
-    ADD FOREIGN KEY (USER_ID)
-        REFERENCES users_scheme.user(id);         
+ALTER TABLE users_scheme.userSkills
+    ADD FOREIGN KEY (ID)
+        REFERENCES users_scheme.user(id)
+        ON DELETE CASCADE;
+        
+ALTER TABLE users_scheme.userInfo
+    ADD FOREIGN KEY (GENDER)
+        REFERENCES users_scheme.gender(id)
+        ON DELETE RESTRICT;
+
+--Индексы
+CREATE INDEX i_city 
+ON users_scheme.userInfo (city);
+
+CREATE INDEX i_gender
+ON users_scheme.userInfo (gender);
+
+CREATE INDEX i_tinder
+ON users_scheme.userInfo  (gender, city);
+
+CREATE INDEX i_nick
+ON users_scheme.userContacts (nickname);
+
+CREATE INDEX i_links
+ON users_scheme.subcriptions (UserPrev, lktype);
