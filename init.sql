718eb5622fb83b455eab8414a90c14907974ed61
AnastasiaZudina <108816201+AnastasiaZudina@users.noreply.github.com>
2022-10-30 00:23:27 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,18 +2,19 @@
 ---CREATE DATABASE users;
 
 -- создание схемы
-CREATE SCHEMA IF NOT EXISTS users_scheme;
+CREATE SCHEMA IF NOT EXISTS users;
 
 --Создание таблиц
-CREATE TABLE IF NOT EXISTS  users_scheme.user(
-    Id uuid PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS  users.user(
+    userId int PRIMARY KEY NOT NULL,
     DELETED bit,
     BEDGA TIMESTAMP NOT NULL,
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.UserInfo(
+CREATE TABLE IF NOT EXISTS  users.UserInfo(
     Id int PRIMARY KEY NOT NULL,
+	userId int not null,
     NAME varchar(120) NOT null,
     FAM varchar(120) NOT null,
     OTCH varchar(120),
@@ -25,8 +26,9 @@ CREATE TABLE IF NOT EXISTS  users_scheme.UserInfo(
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.UserContacts(
-    Id int PRIMARY KEY NOT NULL,    
+CREATE TABLE IF NOT EXISTS  users.UserContacts(
+    Id int PRIMARY KEY NOT NULL, 
+	userId int NOT NULL,	
     NICKNAME varchar(120),
     phoneNum varchar (20) NOT NULL,
     mailAddress varchar(120) NOT NULL,
@@ -35,22 +37,23 @@ CREATE TABLE IF NOT EXISTS  users_scheme.UserContacts(
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.UserPics(
-    Id int PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS  users.UserPics(
+    userId int PRIMARY KEY NOT NULL,
     UPIC varchar(500)
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.UserSkills(
+CREATE TABLE IF NOT EXISTS  users.UserSkills(
     Id int PRIMARY KEY NOT NULL,  
+	userId int NOT NULL,  
     USKILL varchar(120)
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.UserTexts(
-    Id int PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS  users.UserTexts(
+    userId int PRIMARY KEY NOT NULL,
     UTEXT varchar(1000)
 );
 
-CREATE TABLE IF NOT EXISTS  users_scheme.Subscriptions(
+CREATE TABLE IF NOT EXISTS  users.Subscriptions(
 	Id int PRIMARY KEY NOT NULL,
     UserPrev int NOT NULL,
     UserNext int NOT NULL,
@@ -60,7 +63,7 @@ CREATE TABLE IF NOT EXISTS  users_scheme.Subscriptions(
     ENDDA TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS users_scheme.gender(
+CREATE TABLE IF NOT EXISTS users.gender(
     ID varchar (1) PRIMARY KEY NOT NULL,
     G_TEXT varchar(10) NOT NULL
     
@@ -68,49 +71,49 @@ CREATE TABLE IF NOT EXISTS users_scheme.gender(
 
 --Ограничения
 
-ALTER TABLE users_scheme.userInfo
-    ADD FOREIGN KEY (ID)
-        REFERENCES users_scheme.user(id)
+ALTER TABLE users_.userInfo
+    ADD FOREIGN KEY (userId)
+        REFERENCES users.user(userId)
         ON DELETE NO ACTION;
 
-ALTER TABLE users_scheme.userContacts
-    ADD FOREIGN KEY (ID)
-        REFERENCES users_scheme.user(id)
+ALTER TABLE users.userContacts
+    ADD FOREIGN KEY (userId)
+        REFERENCES users.user(userId)
         ON DELETE NO ACTION;
         
-ALTER TABLE users_scheme.userPics
-    ADD FOREIGN KEY (ID)
-        REFERENCES users_scheme.user(id)
+ALTER TABLE users.userPics
+    ADD FOREIGN KEY (userId)
+        REFERENCES users.user(userId)
         ON DELETE CASCADE;     
 
-ALTER TABLE users_scheme.userTexts
-    ADD FOREIGN KEY (ID)
-        REFERENCES users_scheme.user(id)
+ALTER TABLE users.userTexts
+    ADD FOREIGN KEY (userId)
+        REFERENCES users.user(userId)
         ON DELETE CASCADE; 
         
-ALTER TABLE users_scheme.userSkills
-    ADD FOREIGN KEY (ID)
-        REFERENCES users_scheme.user(id)
+ALTER TABLE users.userSkills
+    ADD FOREIGN KEY (userId)
+        REFERENCES users.user(userId)
         ON DELETE CASCADE;
         
 ALTER TABLE users_scheme.userInfo
     ADD FOREIGN KEY (GENDER)
-        REFERENCES users_scheme.gender(id)
+        REFERENCES users.gender(id)
         ON DELETE RESTRICT;
 
 --Индексы
 CREATE INDEX i_city 
-ON users_scheme.userInfo (city);
+ON users.userInfo (city);
 
 CREATE INDEX i_gender
-ON users_scheme.userInfo (gender);
+ON users.userInfo (gender);
 
 CREATE INDEX i_tinder
-ON users_scheme.userInfo  (gender, city);
+ON users.userInfo  (gender, city);
 
 CREATE INDEX i_nick
-ON users_scheme.userContacts (nickname);
+ON users.userContacts (nickname);
 
 CREATE INDEX i_links
-ON users_scheme.subcriptions (UserPrev, lktype);
+ON users.subcriptions (UserPrev, lktype);
 
